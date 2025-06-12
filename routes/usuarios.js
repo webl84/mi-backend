b12cf62064f66f0be2c2939183159a7f954ff15f
Luis morales <luisforever80@gmail.com>
2025-06-12 18:15:02 -0500

    Update usuarios.js

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -53,7 +53,12 @@ res.cookie("auth_token", token, {
       httpOnly: false,
       maxAge: 3600000,
     });
-    
+ res.clearCookie('auth_token', {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'None'
+});
+return res.json({ message: 'Sesión cerrada' });   
 
     // ✅ Devolver el rol también
     res.status(200).json({ 
